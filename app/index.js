9100824e6b38958c23aa3548f7b203b8b93ebf26
eldres <nageljp27@gmail.com>
2019-08-13 08:13:45 -0700

    added missing return statement

--- app/index.js
+++ app/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import './index.css'
 
-class App extends React.Component{
-  render(){
-    <div>
-      
-    </div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
   }
 }
 
-ReactDom.render(
+ReactDOM.render(
   <App />,
   document.getElementById('app')
 )
