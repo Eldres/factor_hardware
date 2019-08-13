8bcd40015eca1426b3f524c96398ad548870b9bd
eldres <nageljp27@gmail.com>
2019-08-13 08:02:20 -0700

    initialized main index for project

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+
+class App extends React.Component{
+  render(){
+    <div>
+      
+    </div>
+  }
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+)
