c7515a6a562f910a7e32902c9c4b4eae492551c1
eldres <nageljp27@gmail.com>
2019-08-14 19:52:43 -0700

    setup layout for results page

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,31 +1,38 @@
 import React from 'react'
 
-export default class Results extends React.Component{
-  render(){
+export default class Results extends React.Component {
+  render() {
     return (
-      <div className='column packageView'>
-      <div className='package-name'>
-        <h1>
-          {this.props.packName}
-        </h1>
-      </div>
-      <div className='package-dueDate'>
-        <h3>Due Date</h3>
-        {this.props.dueBy}
-      </div>
-      <div className='package-files'>
-        <h3>Package Files</h3>
-        {/* {files} */}
-      </div>
-      <div className='package-quantity'>
-        <h3>Requested Quantities</h3>
-        {this.props.unitQuant}
-      </div>
-      <div className='package-desc'>
-        <h3>Description and Notes</h3>
-        {this.props.desc}
-      </div>
-    </div>
+      <React.Fragment>
+        <div className='packageView'>
+          <div className='package-name'>
+            <h1>
+              {this.props.packName}
+            </h1>
+          </div>
+          <div className='package-dueDate'>
+            <h3>Due Date</h3>
+            {this.props.dueBy}
+          </div>
+          <div className='package-files'>
+            <h3>Package Files</h3>
+            {/* {files} */}
+          </div>
+          <div className='package-quantity'>
+            <h3>Requested Quantities</h3>
+            {this.props.unitQuant}
+          </div>
+          <div className='package-desc'>
+            <h3>Description and Notes</h3>
+            {this.props.desc}
+          </div>
+        </div>
+        <button
+          onClick={this.props.onReset}
+          className='btn dark-btn btn-space'>
+          New Package
+        </button>
+      </React.Fragment>
     )
   }
 }
